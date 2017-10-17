5ccae37c8a2f950bc8525118fd95d59658d1dee4
dqf <897432098@qq.com>
2017-10-17 14:23:24 +0800

    增加新的功能

--- QFScanProj/QFScanProj.h
+++ QFScanProj/QFScanProj.h
@@ -8,8 +8,53 @@
 
 #import "QFFileHelper.h"
 
+#define KFolderPath @"/Users/issuser/Desktop/WorkPlace/Test-MiguMusic-iOS/MGMobileMusic"
+
 @interface QFScanProj : NSObject
 
 + (void)scanFunc:(NSString *)path finish:(void(^)(NSArray *allFunc))callback;
 
+
++ (void)scanPng:(NSString *)path finish:(void(^)(NSArray *allPng))callback;
++ (void)scanPNG:(NSString *)path finish:(void(^)(NSArray *allPNG))callback;
+
++ (void)scanJpg:(NSString *)path finish:(void(^)(NSArray *allJpg))callback;
++ (void)scanJPG:(NSString *)path finish:(void(^)(NSArray *allJPG))callback;
+
++ (void)scanJpeg:(NSString *)path finish:(void(^)(NSArray *allJpeg))callback;
++ (void)scanJPEG:(NSString *)path finish:(void(^)(NSArray *allJPEG))callback;
+
+
++ (void)scanGif:(NSString *)path finish:(void(^)(NSArray *allGif))callback;
++ (void)scanGIF:(NSString *)path finish:(void(^)(NSArray *allGIF))callback;
+
++ (void)scanSvg:(NSString *)path finish:(void(^)(NSArray *allSvg))callback;
++ (void)scanSVG:(NSString *)path finish:(void(^)(NSArray *allSVG))callback;
+
++ (void)scanPdf:(NSString *)path finish:(void(^)(NSArray *allPdf))callback;
++ (void)scanPDF:(NSString *)path finish:(void(^)(NSArray *allPDF))callback;
+
+
++ (void)scanWebp:(NSString *)path finish:(void(^)(NSArray *allWebp))callback;
+
+
++ (void)scan1X:(NSString *)path finish:(void(^)(NSArray *all1X))callback;
++ (void)scan2X:(NSString *)path finish:(void(^)(NSArray *all2X))callback;
++ (void)scan3X:(NSString *)path finish:(void(^)(NSArray *all3X))callback;
+
++ (void)scanUseX:(NSString *)path finish:(void(^)(NSArray *allUseX))callback;
++ (void)scanUsex:(NSString *)path finish:(void(^)(NSArray *allUsex))callback;
+
+
++ (void)scanUnusedImages:(NSString *)path finish:(void(^)(NSArray *allUnusedImages))callback;
+
++ (void)scanPlist:(NSString *)path finish:(void(^)(NSArray *allPlist))callback;
+
+
++ (void)scanMP3:(NSString *)path finish:(void(^)(NSArray *allMP3))callback;
++ (void)scanMP4:(NSString *)path finish:(void(^)(NSArray *allMP4))callback;
++ (void)scanWov:(NSString *)path finish:(void(^)(NSArray *allWov))callback;
++ (void)scanFlv:(NSString *)path finish:(void(^)(NSArray *allFlv))callback;
+
 @end
+
--- QFScanProj/QFScanProj.m
+++ QFScanProj/QFScanProj.m
@@ -12,16 +12,397 @@
 @implementation QFScanProj
 
 + (void)scanFunc:(NSString *)path finish:(void(^)(NSArray *allFunc))callback {
-    NSMutableArray *allFunc = [NSMutableArray new];
+    NSMutableArray *mutableArr = [NSMutableArray new];
     [QFFileHelper folderPath1:path filter:@".m" block:^(NSString *path) {
         NSArray *arr = [[QFCheckFunc share] checkFunc:path];
         if (arr && arr.count > 0) {
-            [allFunc addObjectsFromArray:arr];
+            [mutableArr addObjectsFromArray:arr];
         }
     }];
     if (callback) {
-        callback(allFunc);
+        callback(mutableArr);
+    }
+}
+
++ (void)scanPng:(NSString *)path finish:(void(^)(NSArray *allPng))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".png" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanPNG:(NSString *)path finish:(void(^)(NSArray *allPNG))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".PNG" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanJpg:(NSString *)path finish:(void(^)(NSArray *allJpg))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".jpg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanJPG:(NSString *)path finish:(void(^)(NSArray *allJPG))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".JPG" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanJpeg:(NSString *)path finish:(void(^)(NSArray *allJpeg))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".jpeg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanJPEG:(NSString *)path finish:(void(^)(NSArray *allJPEG))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".JPEG" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanGif:(NSString *)path finish:(void(^)(NSArray *allGif))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".git" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanGIF:(NSString *)path finish:(void(^)(NSArray *allGIF))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".GIF" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanSvg:(NSString *)path finish:(void(^)(NSArray *allSvg))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".svg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanSVG:(NSString *)path finish:(void(^)(NSArray *allSVG))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".SVG" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanPdf:(NSString *)path finish:(void(^)(NSArray *allPdf))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".pdf" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanPDF:(NSString *)path finish:(void(^)(NSArray *allPDF))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".PDF" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanWebp:(NSString *)path finish:(void(^)(NSArray *allWebp))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".Webp" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scan1X:(NSString *)path finish:(void(^)(NSArray *all1X))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@"@1X.png" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    [QFFileHelper folderPath1:path filter:@"@1X.jpg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scan2X:(NSString *)path finish:(void(^)(NSArray *all2X))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@"@2X.png" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    [QFFileHelper folderPath1:path filter:@"@2X.jpg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scan3X:(NSString *)path finish:(void(^)(NSArray *all3X))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@"@3X.png" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    [QFFileHelper folderPath1:path filter:@"@3X.jpg" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanUseX:(NSString *)path finish:(void(^)(NSArray *allUseX))callback {
+
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    
+    [QFFileHelper folderPath1:path filter:@".h" block:^(NSString *path) {
+        
+        __block BOOL contain = NO;
+        
+        [QFFileHelper file:path block:^(NSString *lineStr) {
+            
+            NSArray *filterArr = @[@"@1X.png", @"@2X.png", @"@3X.png", @"@1X.jpg", @"@2X.jpg", @"@3X.jpg"];
+            if (lineStr.containsStrArr(filterArr)) {
+                contain = YES;
+            }
+        }];
+        if (contain) {
+            //不包含
+            [mutableArr addObject:path];
+        }
+    }];
+    [QFFileHelper folderPath1:path filter:@".m" block:^(NSString *path) {
+        
+        __block BOOL contain = NO;
+        
+        [QFFileHelper file:path block:^(NSString *lineStr) {
+            
+            NSArray *filterArr = @[@"@1X.png", @"@2X.png", @"@3X.png", @"@1X.jpg", @"@2X.jpg", @"@3X.jpg"];
+            if (lineStr.containsStrArr(filterArr)) {
+                contain = YES;
+            }
+        }];
+        if (contain) {
+            //不包含
+            [mutableArr addObject:path];
+        }
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanUsex:(NSString *)path finish:(void(^)(NSArray *allUsex))callback {
+
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    
+    [QFFileHelper folderPath1:path filter:@".h" block:^(NSString *path) {
+        
+        __block BOOL contain = NO;
+        
+        [QFFileHelper file:path block:^(NSString *lineStr) {
+            
+            NSArray *filterArr = @[@"@1x.png", @"@2x.png", @"@3x.png", @"@1x.jpg", @"@2x.jpg", @"@3x.jpg"];
+            if (lineStr.containsStrArr(filterArr)) {
+                contain = YES;
+            }
+        }];
+        if (contain) {
+            //不包含
+            [mutableArr addObject:path];
+        }
+    }];
+    [QFFileHelper folderPath1:path filter:@".m" block:^(NSString *path) {
+        
+        __block BOOL contain = NO;
+        
+        [QFFileHelper file:path block:^(NSString *lineStr) {
+            
+            NSArray *filterArr = @[@"@1x.png", @"@2x.png", @"@3x.png", @"@1x.jpg", @"@2x.jpg", @"@3x.jpg"];
+            if (lineStr.containsStrArr(filterArr)) {
+                contain = YES;
+            }
+        }];
+        if (contain) {
+            //不包含
+            [mutableArr addObject:path];
+        }
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanUnusedImages:(NSString *)path finish:(void(^)(NSArray *allUnusedImages))callback {
+
+    NSMutableArray *mutableArr1 = [NSMutableArray array];
+    NSMutableArray *mutableArr2 = [NSMutableArray array];
+    NSMutableArray *allFilterArr = [NSMutableArray array];
+    
+    [QFFileHelper folderPath1:path filterArr:@[@".png", @".jgp"] block:^(NSString *path) {
+        [mutableArr1 addObject:path];
+    }];
+    
+    [QFFileHelper folderPath1:path filterArr:@[@".h", @".m", @".mm", @".c", @".cpp", @".xib", @".json"] block:^(NSString *path) {
+        [allFilterArr addObject:path];
+    }];
+    
+    NSUInteger count = allFilterArr.count;
+    
+    for (int i=0; i<count; i++) {
+        
+        NSLog(@"%lu/%d", count,i);
+        NSString *path = allFilterArr[i];
+        NSString *contentStr = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+        
+        for (NSString *imgStr in mutableArr1) {
+            
+            NSString *tmpImgStr = [imgStr lastPathComponent].replace(@"@1x", @"").replace(@"@2x", @"").replace(@"@3x", @"");
+            NSString *str1 = nil;
+            NSString *str2 = nil;
+            NSString *str3 = nil;
+            
+            if ([[path lastPathComponent] hasSuffix:@".xib"] || [[path lastPathComponent] hasSuffix:@".json"]) {
+                
+                str1 = NSString.append(@"\"").append([imgStr lastPathComponent]).append(@"\"");
+                str2 = NSString.append(@"\"").append(tmpImgStr).append(@"\"");
+                
+                if ([imgStr hasSuffix:@"png"]) {
+                    str3 = NSString.append(@"\"").append(tmpImgStr).append(@"\"").replace(@".png", @"");
+                }else if ([imgStr hasSuffix:@"jpg"]) {
+                    str3 = NSString.append(@"\"").append(tmpImgStr).append(@"\"").replace(@".jpg", @"");
+                }
+                
+            }else {
+                
+                str1 = NSString.append(@"UIImage imageNamed:@\"").append([imgStr lastPathComponent]).append(@"\"");
+                str2 = NSString.append(@"UIImage imageNamed:@\"").append(tmpImgStr).append(@"\"");
+                
+                if ([imgStr hasSuffix:@"png"]) {
+                    str3 = NSString.append(@"UIImage imageNamed:@\"").append(tmpImgStr).append(@"\"").replace(@".png", @"");
+                }else if ([imgStr hasSuffix:@"jpg"]) {
+                    str3 = NSString.append(@"UIImage imageNamed:@\"").append(tmpImgStr).append(@"\"").replace(@".jpg", @"");
+                }
+                
+            }
+            
+            BOOL containsObject = NO;
+            if (str1 && [contentStr containsString:str1]) {
+                containsObject = YES;
+            }else if (str2 && [contentStr containsString:str2]) {
+                containsObject = YES;
+            }else if (str3 && [contentStr containsString:str3]) {
+                containsObject = YES;
+            }
+            
+            if (containsObject && ![mutableArr2 containsObject:imgStr]) {
+                [mutableArr2 addObject:imgStr];
+            }
+        }
+    }
+    
+    for (NSString *imgStr in mutableArr2) {
+        if ([mutableArr1 containsObject:imgStr]) {
+            [mutableArr1 removeObject:imgStr];
+        }
+    }
+    
+    if (callback) {
+        callback(mutableArr1);
+    }
+}
+
++ (void)scanPlist:(NSString *)path finish:(void(^)(NSArray *allPlist))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".plist" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanMP3:(NSString *)path finish:(void(^)(NSArray *allMP3))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".mp3" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanMP4:(NSString *)path finish:(void(^)(NSArray *allMP4))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".mp4" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanWov:(NSString *)path finish:(void(^)(NSArray *allWov))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".wov" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
+    }
+}
+
++ (void)scanFlv:(NSString *)path finish:(void(^)(NSArray *allFlv))callback {
+    NSMutableArray *mutableArr = [NSMutableArray array];
+    [QFFileHelper folderPath1:path filter:@".flv" block:^(NSString *path) {
+        [mutableArr addObject:path];
+    }];
+    if (callback) {
+        callback(mutableArr);
     }
 }
 
 @end
+
